a90625755d242262e9c0917b71377b15d045e1cc
PrajwalKarande14 <prajwal_rajendrakarande@epam.com>
2026-04-02 21:35:25 +0530

    Stats

--- src/routes/statRoutes.ts
+++ src/routes/statRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { authenticate, authorizeAdmin } from "../middleware/auth.js";
+import { authenticate } from "../middleware/auth.js";
 import { getStats } from "../controllers/statsController.js";
 
 const router = Router()
 
-router.get("/", authenticate,authorizeAdmin, getStats)
+router.get("/", authenticate, getStats)
 
 export default router
